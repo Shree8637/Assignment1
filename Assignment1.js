4162378df02154b87e2e800062f43565e2f3188a
Shreeraj <shreejagtap231@gmail.com>
2024-06-24 12:20:18 +0530

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,68 @@
+// 1.  Write a program that defines and logs different data types
+// var a = 'Sam'        // declare a string
+// var b = 12           // declare a number
+// var c = true         // declare a boolean
+// var d = null         // declare a object
+// var e = [1, 2, 3]    // declare an array
+// console.log(a, typeof a)     // typeof keyword gives the type of specific variable
+// console.log(b, typeof b)
+// console.log(c, typeof c)
+// console.log(d, typeof d)
+// console.log(e, typeof e)
+
+
+// 2. Write a program that performs basic arithmetic operations
+// var a = Number(prompt('Enter a first number: '))     // Taking User input
+// var b = Number(prompt('Enter a second number: '))    // Taking User input
+// console.log('Addition: ', a + b)
+// console.log('Subtraction: ', a - b)
+// console.log('Multiplication: ',a * b)
+// console.log('Division: ', a / b)
+
+
+// // 3. Create a program that checks if a number is positive, negative, or zero.
+// var a = Number(prompt('Enter a Number: '))       // User input
+// if (a == 0) {                                    // check if number is zero
+//     console.log('User input is a Zero')
+// }
+// else if (a > 0) {                                // check if number is positive
+//     console.log('User input is a Positive Number')
+// }
+// else if (a < 0) {                                // check if number is negative
+//     console.log('User input is a Negative Number' )
+// }
+// else {                                           // check for any other input other than a number
+//     console.log('Invalid User input')
+// }
+
+
+// 4. Write a program that prints all even numbers from 1 to 20
+// for (i = 1; i < 21; i++) {
+//     if (i % 2 == 1) {
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+// 5. Create a program that simulates a simple grading system
+// var a = prompt('Enter a Grade (0-100): ')
+// if (a >= 90 && a <= 100) {
+//     console.log('A')
+// }
+// else if (a >= 80 && a <= 89 ) {
+//     console.log('B')
+// }
+// else if (a >= 70 && a <= 79) {
+//     console.log('C')
+// }
+// else if (a >= 60 && a <= 69) {
+//     console.log('D')
+// }
+// else if (a >= 0 && a <= 59) {
+//     console.log('F')
+// }
+// else {
+//     console.log('Invalid Grade')
+// }
+
